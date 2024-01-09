adc4dba1934ced3190b874a947d8437122d4b83f
Renāte <spirgarenate@gmail.com>
2024-01-09 13:51:56 +0200

    info modal changes

--- src/components/InfoModal.js
+++ src/components/InfoModal.js
@@ -23,7 +23,7 @@ export default function InfoModal({ handler, display }: InfoModalProps): React$M
       <div className={mobile ? "ModalBox ModalBox-mobile" : "ModalBox"}>
         <p dangerouslySetInnerHTML={{ __html: ModalText }} />
         <p style={{ fontSize: "14px" }}>
-          Re-made for Paramore and Hayley Williams by&nbsp;<a href="http://renaates.github.io">Renee.</a> If you have comments or
+          Re-made for Paramore and Hayley Williams by Renee. If you have comments or
           suggestions, my Twitter is <a href="https://twitter.com/renaateste">@renaateste</a>!
         </p>
         {/** CREDITS: Please do not edit this. Feel free to add your own credits to ModalText. */}
--- src/components/constants.js
+++ src/components/constants.js
@@ -5,7 +5,7 @@ export const ModalText =
   "Search for a word or a phrase and see all the places Paramore or Hayley Williams \
   has used it in their music! To search for multiple words or phrases, use \
   a comma between them. Use a * for wildcard search. \
-  <br><br>Last updated: 09/10/23. First published 09/10/23.";
+  <br><br>Last updated: 09/01/24. First published 09/10/23.";
 
 // Ordered list of albums for selector - keep in sync with album map
 export const ALBUMS = [
